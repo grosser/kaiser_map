bfc2197ff97407d4736234f39061f6d52e1b93f0
grosser <grosser.michael@gmail.com>
2011-12-01 21:42:17 -0800

    first parsing version

--- lib/kaiser_map/parser.rb
+++ lib/kaiser_map/parser.rb
@@ -1,19 +1,56 @@
-module KaiserMap::Parser
-  def self.parse(zip)
-    def log(x)
-      File.open('xxx.html','w'){|f| f.write x }
+# encoding: UTF-8
+
+module KaiserMap
+  module Parser
+    def self.parse(zip)
+      pages = all_pages(zip)
+      parse_pages(pages)
     end
 
-    agent = Mechanize.new
+    def self.parse_pages(pages)
+      pages.map do |page|
+        page.search('.doctorInfo').map do |doctor|
+          parse_doctor(doctor)
+        end
+      end.flatten(2)
+    end
 
-    # login
-    search = agent.post 'https://mydoctor.kaiserpermanente.org/cyd/nonMemberSearch.action';nil
-    form = search.form('searchForm');nil
-    form.zipcode = zip
-    form.radiobuttons_with(:name => 'speciality')[0].check # Adult medicine
-    form.distance = '1' # 15 miles
-    results = agent.submit form;nil
-    puts results.body
+    def self.parse_doctor(doctor)
+      puts 1
+      {
+        :name => doctor.search('.title').text,
+        :url => doctor.search('.learnMore').first['href'],
+        :address => doctor.search('.mt5').text.gsub("\302\240",' ').gsub(/\s+/,' '),
+        :image_url => doctor.search('img').first['src']
+      }
+    end
 
+    def self.all_pages(zip)
+      agent = Mechanize.new
+
+      # login
+      page = agent.post 'https://mydoctor.kaiserpermanente.org/cyd/nonMemberSearch.action';nil
+      form = page.form('searchForm');nil
+
+      # post initial search values
+      first = agent.post 'https://mydoctor.kaiserpermanente.org/cyd/ajaxNonMemberSearchResults.action',
+        "distance" =>	"1",
+        "kaiserKey" => form.kaiserKey,
+        "location" => "",
+        "securityKey" => form.securityKey,
+        "speciality" => "MED", # Adult medicine
+        "zipcode" => "94103"
+
+      # get all pages
+      per_page = 10
+      pages = first.body.match(/of\s+(\d+)/)[1].to_i / per_page
+      pages = Array.new(pages)
+
+      Parallel.map_with_index(pages, :in_threads => 4) do |page, index|
+        page = index + 1
+        random = "0.#{9999999999999999}"
+        agent.get "https://mydoctor.kaiserpermanente.org/cyd/ajaxLoadSearchResults.action?pageAction=&pageNumber=#{page}&securityKey=#{form.securityKey}&sid=#{random}"
+      end
+    end
   end
 end
--- spec/kaiser_map/parser_spec.rb
+++ spec/kaiser_map/parser_spec.rb
@@ -10,4 +10,18 @@ describe KaiserMap::Parser do
     results.first[:image_url].should =~ /^http/
     results.first[:name].should.size.should > 10
   end
+
+  describe :parse_pages do
+    it "reads all doctors" do
+      html = Nokogiri::HTML(File.read('spec/fixtures/full_page.html'))
+      results = KaiserMap::Parser.parse_pages([html])
+      results.size.should == 10
+      results.first.should == {
+        :name=>"Joseph Atkinson, MD",
+        :url=>"http://www.kp.org/mydoctor/atkinson?tab=about/professional",
+        :address=>" Medicine 2200 O'farrell Street San Francisco,CA 94115 ",
+        :image_url=>"https://www.permanente.net/pmdb/photosync/8266130_photoweb.jpg"
+      }
+    end
+  end
 end
